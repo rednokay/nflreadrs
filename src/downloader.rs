a076c4dfc436993e783510eee8f81371c3b54ed5
rednokay <207755534+rednokay@users.noreply.github.com>
2026-01-02 07:37:33 +0100

    feat: add public function for downloading files
    Add a public function to download data to a specific location.

--- src/downloader.rs
+++ src/downloader.rs
@@ -75,3 +75,69 @@ where
     let path_to_file = fetch_content(downloader)?;
     from_csv(path_to_file, None)
 }
+
+/// Create headers for the get request on GitHub,
+fn create_headers() -> HeaderMap {
+    let mut headers = HeaderMap::new();
+    headers.append(USER_AGENT, HeaderValue::from_static("nflreadrs"));
+    headers.append(
+        ACCEPT,
+        HeaderValue::from_static("application/vnd.github+json"),
+    );
+    headers.append(
+        "X-GitHub-Api-Version",
+        HeaderValue::from_static("2022-11-28"),
+    );
+    return headers;
+}
+
+/// Called on a downloader to download data to a specified path.
+///
+/// If the the specified path is a dict, a UUID based name will be given to the file.
+///
+/// # Arguments
+///
+/// * `downloader`  -   The struct relating to the desired stats. Needs to implement Downloader.
+/// * `path`        -   Destination path. If none, the file will be downloaded to the temporary dictionary.
+/// * `force`       -   Overwrites exiting files if true. If None or false returns Err and does not write the file.
+pub fn download_to<D>(
+    downloader: &D,
+    path: Option<&PathBuf>,
+    force: Option<bool>,
+) -> Result<PathBuf>
+where
+    D: Downloader,
+{
+    let mut save_to: PathBuf;
+
+    if let Some(p) = path {
+        if p.exists()
+            && match force {
+                Some(v) => !v,
+                None => true,
+            }
+        {
+            anyhow::bail!(
+                "This file already exists. If you want to overwrite it use force = Some(true)"
+            );
+        }
+        save_to = p.clone();
+    } else {
+        save_to = env::temp_dir();
+    }
+
+    if save_to.is_dir() {
+        let id = Uuid::new_v4().to_string();
+        save_to.push(format!("nflreadrs-{}.csv", &id));
+    }
+
+    let mut file = File::create(&save_to)?;
+
+    let client = blocking::Client::new();
+    let headers = create_headers();
+    let mut response = client.get(downloader.url()?).headers(headers).send()?;
+
+    response.copy_to(&mut file)?;
+
+    Ok(save_to)
+}

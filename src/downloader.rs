a0c253c825d385cc45c72e7a5f1fb79e6e1232f1
rednokay <207755534+rednokay@users.noreply.github.com>
2026-01-08 09:01:50 +0100

    refactor: Downloader trait overhaul
    ATM this is not functional.

--- src/downloader.rs
+++ src/downloader.rs
@@ -13,6 +13,79 @@ use uuid::Uuid;
 pub trait Downloader {
     /// Returns the URL path for this downloader.
     fn url(&self) -> Result<Url>;
+
+    /// Create headers for the get request on GitHub.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use nflreadrs::{stats::Rosters, downloader::Downloader};
+    ///
+    /// let headers = Rosters::headers();
+    ///
+    /// assert!(!headers.is_empty());
+    /// ```
+    fn headers() -> HeaderMap {
+        let mut headers = HeaderMap::new();
+        headers.append(USER_AGENT, HeaderValue::from_static("nflreadrs"));
+        headers.append(
+            ACCEPT,
+            HeaderValue::from_static("application/vnd.github+json"),
+        );
+        headers.append(
+            "X-GitHub-Api-Version",
+            HeaderValue::from_static("2022-11-28"),
+        );
+        headers
+    }
+
+    /// Initialize the get client
+    fn client(&self) -> blocking::Client {
+        blocking::Client::builder()
+            .default_headers(Self::headers())
+            .build()
+            .unwrap()
+    }
+
+    /// Download data to a specified path.
+    ///
+    /// # Arguments
+    ///
+    /// * `path`        -   Destination path. If none, the file will be downloaded to the temporary dictionary.
+    /// * `force`       -   Overwrites exiting files if true. If None or false returns Err and does not write the file.
+    fn download(&self, path: Option<&PathBuf>, force: Option<bool>) -> Result<PathBuf> {
+        let mut save_to: PathBuf;
+
+        if let Some(p) = path {
+            if p.exists()
+                && match force {
+                    Some(v) => !v,
+                    None => true,
+                }
+            {
+                anyhow::bail!(
+                    "This file already exists. If you want to overwrite it use force = Some(true)"
+                );
+            }
+            save_to = p.clone();
+        } else {
+            save_to = env::temp_dir();
+        }
+
+        if save_to.is_dir() {
+            let id = Uuid::new_v4().to_string();
+            save_to.push(format!("nflreadrs-{}.csv", &id));
+        }
+
+        let mut file = File::create(&save_to)?;
+
+        let client = self.client();
+        let mut response = client.get(self.url()?).send()?;
+
+        response.copy_to(&mut file)?;
+
+        Ok(save_to)
+    }
 }
 
 /// Reads a downloaded CSV file to DataFrame.
@@ -36,72 +109,57 @@ pub fn pull<D>(downloader: &D) -> Result<DataFrame>
 where
     D: Downloader,
 {
-    let path_to_file = download_to(downloader, None, None)?;
+    let path_to_file = downloader.download(None, None)?;
     from_csv(path_to_file, None)
 }
 
-/// Create headers for the get request on GitHub,
-fn create_headers() -> HeaderMap {
-    let mut headers = HeaderMap::new();
-    headers.append(USER_AGENT, HeaderValue::from_static("nflreadrs"));
-    headers.append(
-        ACCEPT,
-        HeaderValue::from_static("application/vnd.github+json"),
-    );
-    headers.append(
-        "X-GitHub-Api-Version",
-        HeaderValue::from_static("2022-11-28"),
-    );
-    headers
-}
+// /// Called on a downloader to download data to a specified path.
+// ///
+// /// If the the specified path is a dict, a UUID based name will be given to the file.
+// ///
+// /// # Arguments
+// ///
+// /// * `downloader`  -   The struct relating to the desired stats. Needs to implement Downloader.
+// /// * `path`        -   Destination path. If none, the file will be downloaded to the temporary dictionary.
+// /// * `force`       -   Overwrites exiting files if true. If None or false returns Err and does not write the file.
+// pub fn download_to<D>(
+//     downloader: &D,
+//     path: Option<&PathBuf>,
+//     force: Option<bool>,
+// ) -> Result<PathBuf>
+// where
+//     D: Downloader,
+// {
+//     let mut save_to: PathBuf;
 
-/// Called on a downloader to download data to a specified path.
-///
-/// If the the specified path is a dict, a UUID based name will be given to the file.
-///
-/// # Arguments
-///
-/// * `downloader`  -   The struct relating to the desired stats. Needs to implement Downloader.
-/// * `path`        -   Destination path. If none, the file will be downloaded to the temporary dictionary.
-/// * `force`       -   Overwrites exiting files if true. If None or false returns Err and does not write the file.
-pub fn download_to<D>(
-    downloader: &D,
-    path: Option<&PathBuf>,
-    force: Option<bool>,
-) -> Result<PathBuf>
-where
-    D: Downloader,
-{
-    let mut save_to: PathBuf;
+//     if let Some(p) = path {
+//         if p.exists()
+//             && match force {
+//                 Some(v) => !v,
+//                 None => true,
+//             }
+//         {
+//             anyhow::bail!(
+//                 "This file already exists. If you want to overwrite it use force = Some(true)"
+//             );
+//         }
+//         save_to = p.clone();
+//     } else {
+//         save_to = env::temp_dir();
+//     }
 
-    if let Some(p) = path {
-        if p.exists()
-            && match force {
-                Some(v) => !v,
-                None => true,
-            }
-        {
-            anyhow::bail!(
-                "This file already exists. If you want to overwrite it use force = Some(true)"
-            );
-        }
-        save_to = p.clone();
-    } else {
-        save_to = env::temp_dir();
-    }
+//     if save_to.is_dir() {
+//         let id = Uuid::new_v4().to_string();
+//         save_to.push(format!("nflreadrs-{}.csv", &id));
+//     }
 
-    if save_to.is_dir() {
-        let id = Uuid::new_v4().to_string();
-        save_to.push(format!("nflreadrs-{}.csv", &id));
-    }
+//     let mut file = File::create(&save_to)?;
 
-    let mut file = File::create(&save_to)?;
+//     let client = blocking::Client::new();
+//     let headers = create_headers();
+//     let mut response = client.get(downloader.url()?).headers(headers).send()?;
 
-    let client = blocking::Client::new();
-    let headers = create_headers();
-    let mut response = client.get(downloader.url()?).headers(headers).send()?;
+//     response.copy_to(&mut file)?;
 
-    response.copy_to(&mut file)?;
-
-    Ok(save_to)
-}
+//     Ok(save_to)
+// }
